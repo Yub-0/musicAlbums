94d7cc0c0ae81eccc761419251b8259d26be08b8
Yub <yubmaharjan@gmail.com>
2022-03-23 14:00:59 +0545

    adding AddTrackViewSet

--- firstProject/projects/urls.py
+++ firstProject/projects/urls.py
@@ -1,36 +1,47 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from projects.views import ListAlbumViewSet, TrackViewSet
+from projects.views import ListAlbumViewSet, ListSpecificAlbum, AddAlbumViewSet, ListTrackViewSet, ListSpecificTrack, AddTrackViewSet
+
 
 album_list = ListAlbumViewSet.as_view({
     'get': 'list',
+})
+album_list_specific = ListSpecificAlbum.as_view({
+    'get': 'retrieve'
+})
+album_add = AddAlbumViewSet.as_view({
+    'post': 'create'
+})
+
+track_list = ListTrackViewSet.as_view({
+    'get': 'list',
+})
+track_list_specific = ListSpecificTrack.as_view({
     'get': 'retrieve',
 })
-# album_add = AddAlbumViewSet.as_view({
-#     'post': 'create'
-# })
+track_add = AddTrackViewSet.as_view({
+    'post': 'create'
+})
 # album_detail = LAlbumViewSet.as_view({
 #     'get': 'retrieve',
 #     'put': 'update',
 #     'patch': 'partial_update',
 #     'delete': 'destroy'
 # })
-track_list = TrackViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-track_detail = TrackViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
+
+# track_detail = TrackViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
 
 urlpatterns = format_suffix_patterns([
     path('albums/', album_list, name='album-list'),
-    path('albums/<int:pk>/', album_list, name='album-detail'),
-    # path('addAlbums', album_add, name='album_add'),
+    path('albums/<int:pk>/', album_list_specific, name='album-detail'),
+    path('addAlbums', album_add, name='album_add'),
     path('tracks/', track_list, name='track-list'),
-    path('tracks/<int:pk>/', track_detail, name='track-detail')
+    path('tracks/<int:pk>/', track_list_specific, name='track-detail'),
+    path('addTracks', track_add, name='track_add'),
 ])
--- firstProject/projects/views.py
+++ firstProject/projects/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, status
 
 from projects.models import Album, Track
 from projects.serializers import AlbumSerializer, TrackSerializer
@@ -20,27 +20,53 @@ class ListAlbumViewSet(viewsets.ViewSet):
         serializer = AlbumSerializer(queryset, many=True)
         return Response(serializer.data)
 
+
+class ListSpecificAlbum(viewsets.ViewSet):
     def retrieve(self, request, pk=None):
         queryset = Album.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = AlbumSerializer(user)
+        album = get_object_or_404(queryset, pk=pk)
+        serializer = AlbumSerializer(album)
         return Response(serializer.data)
 
 
-# class AddAlbumViewSet(viewsets.ViewSet):
-#     def create(self, request, *args, **kwargs):
-#         serializer = AlbumSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save(serializer)
-    # def create(self, request):
-        # serializer = AlbumSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     serializer.save()
-        #     return JsonResponse(serializer.data, status=201)
-        # return JsonResponse(serializer.errors, status=400)
+class AddAlbumViewSet(viewsets.ViewSet):
+    def create(self, request, *args, **kwargs):
+        serializer = AlbumSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # def create(self, request, *args, **kwargs):
+    #
+    #     serializer = AlbumSerializer(data=request.data)
+    #     print("here hello")
+    #     if serializer.is_valid(raise_exception=True):
+    #         serializer.save(serializer)
+    #         return JsonResponse(serializer.data, status=201)
+    #     return JsonResponse(serializer.errors, status=400)
 
 
-class TrackViewSet(viewsets.ModelViewSet):
+class AddTrackViewSet(viewsets.ViewSet):
+    def create(self, request, *args, **kwargs):
+        serializer = TrackSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-    queryset = Track.objects.all()
-    serializer_class = TrackSerializer
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ListTrackViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Track.objects.all()
+        serializer = TrackSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class ListSpecificTrack(viewsets.ViewSet):
+    def retrieve(self, request, pk=None):
+        queryset = Track.objects.all()
+        track = get_object_or_404(queryset, pk=pk)
+        serializer = TrackSerializer(track)
+        return Response(serializer.data)
+
